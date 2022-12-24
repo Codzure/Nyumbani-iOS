0ac4123b1095856861ee2f33e6902c186ab64e71
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2022-12-24 23:59:27 +0300

    Initial Commit

---
+++ Nyumbani-iOS/Nyumbani_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nyumbani_iOSApp.swift
+//  Nyumbani-iOS
+//
+//  Created by Leonard Mutugi on 24/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Nyumbani_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
